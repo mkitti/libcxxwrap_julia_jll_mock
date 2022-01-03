5bcb1e47134b9df0890a61a69a15f4b66fa629fb
Mark Kittisopikul <markkitt@gmail.com>
2022-01-03 03:29:31 -0500

    Generalize to setup

--- src/libcxxwrap_julia_jll.jl
+++ src/libcxxwrap_julia_jll.jl
@@ -1,6 +1,9 @@
 baremodule libcxxwrap_julia_jll
     export libcxxwrap_julia, libcxxwrap_julia_stl
-    const libcxxwrap_julia = "/home/mkitti/anaconda3/envs/julia171_test/share/julia/artifacts/6fbda767b0cb63d5cd1b258fd698b8fb14d655c0/lib/libcxxwrap_julia.so"
+    # https://github.com/JuliaBinaryWrappers/libcxxwrap_julia_jll.jl/blob/700a4ed56763315cd6e9120216af0ef2449d3c7c/Artifacts.toml#L657
+    # x86_64, cxx11, 1.7.0, glibc, linux
+    const artifact_git_tree_sha1 = "6fbda767b0cb63d5cd1b258fd698b8fb14d655c0"
+    const libcxxwrap_julia = "$(DEPOT_PATH[1])/artifacts/$artifact_git_tree_sha1/lib/libcxxwrap_julia.so"
     const libcxxwrap_julia_path = libcxxwrap_julia
-    const libcxxwrap_julia_stl = "/home/mkitti/anaconda3/envs/julia171_test/share/julia/artifacts/6fbda767b0cb63d5cd1b258fd698b8fb14d655c0/lib/libcxxwrap_julia_stl.so"
+    const libcxxwrap_julia_stl = "$(DEPOT_PATH[1])/artifacts/$artifact_git_tree_sha1/lib/libcxxwrap_julia_stl.so"
 end
