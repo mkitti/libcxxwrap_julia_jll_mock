3c828967017acbfccae9eda6431312a290a60719
Mark Kittisopikul <markkitt@gmail.com>
2022-01-03 04:30:58 -0500

    Need Base for ENV

--- src/libcxxwrap_julia_jll.jl
+++ src/libcxxwrap_julia_jll.jl
@@ -1,5 +1,6 @@
 baremodule libcxxwrap_julia_jll
     export libcxxwrap_julia, libcxxwrap_julia_stl
+    import Base
     # If we used the remaining artifact from the actual libcxxwrap_julia_jll
     # https://github.com/JuliaBinaryWrappers/libcxxwrap_julia_jll.jl/blob/700a4ed56763315cd6e9120216af0ef2449d3c7c/Artifacts.toml#L657
     # x86_64, cxx11, 1.7.0, glibc, linux
@@ -7,7 +8,7 @@ baremodule libcxxwrap_julia_jll
     # const artifact_location = "$(Base.DEPOT_PATH[1])/artifacts/$artifact_git_tree_sha1"
     
     # Location of untarred https://github.com/JuliaBinaryWrappers/libcxxwrap_julia_jll.jl/releases/download/libcxxwrap_julia-v0.8.8+1/libcxxwrap_julia.v0.8.8.x86_64-linux-gnu-cxx11-julia_version+1.7.0.tar.gz
-    const artifact_location = ENV["CONDA_PREFIX"]
+    const artifact_location = Base.ENV["CONDA_PREFIX"]
     const libcxxwrap_julia = "$artifact_location/lib/libcxxwrap_julia.so"
     const libcxxwrap_julia_path = libcxxwrap_julia
     const libcxxwrap_julia_stl = "$artifact_location/lib/libcxxwrap_julia_stl.so"
