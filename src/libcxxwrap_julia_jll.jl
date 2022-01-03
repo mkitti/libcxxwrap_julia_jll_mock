1ba77cdaabc68240adeeceedc5880049ac53fb4f
Mark Kittisopikul <markkitt@gmail.com>
2022-01-03 03:05:16 -0500

    Mock libcxxwrap_julia_path

--- src/libcxxwrap_julia_jll.jl
+++ src/libcxxwrap_julia_jll.jl
@@ -1,5 +1,6 @@
 baremodule libcxxwrap_julia_jll
     export libcxxwrap_julia, libcxxwrap_julia_stl
     const libcxxwrap_julia = "/home/mkitti/anaconda3/envs/julia171_test/share/julia/artifacts/6fbda767b0cb63d5cd1b258fd698b8fb14d655c0/lib/libcxxwrap_julia.so"
+    const libcxxwrap_julia_path = libcxxwrap_julia
     const libcxxwrap_julia_stl = "/home/mkitti/anaconda3/envs/julia171_test/share/julia/artifacts/6fbda767b0cb63d5cd1b258fd698b8fb14d655c0/lib/libcxxwrap_julia_stl.so"
 end
